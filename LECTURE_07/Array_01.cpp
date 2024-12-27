df08ccccade13f5cceea6c8fcb81c4e40e49776c
Sahil-sharma-30 <sahilsharma3043@gmail.com>
2024-12-27 13:13:10 +0530

    Add index tracking for minimum and maximum values in array

--- LECTURE_07/Array_01.cpp
+++ LECTURE_07/Array_01.cpp
@@ -4,11 +4,13 @@ int main(){
 int arr[6]={120,86,52,68,71};
 
 int min = arr[0];
+int alpha=0;
 for (int i = 0; i < 5; i++)
 {
     if (arr[i] < min)
     {
         min = arr[i];
+        alpha=i;
     }
     else
     {
@@ -18,15 +20,19 @@ for (int i = 0; i < 5; i++)
 cout <<" Tme minimum value in the array is : "<< min;
 
 int max = arr[0];
+int beta=0;
 for (int  j = 0; j <5; j++)
 {
 if ( arr[j] > max )
 {
     max = arr[j];
-}
-}
+    beta=j;
 
+}
+}
 cout <<"\n The maximum element in the array is : "<< max;
+cout <<"\nThe maximum element is present at the index : "<< alpha;
+cout<<"\nThe minimum element is present at the index : "<< beta;
 
     return 0;
 }
